1e5b30458e73fffc641232715059a01599ada131
ggkounta <georgiagount@gmail.com>
2025-09-07 13:22:44 +0300

    test

---
+++ QuadD.go
@@ -0,0 +1,30 @@
+package quad
+
+import "fmt"
+
+func QuadD(x, y int) {
+	if x <= 0 || y <= 0 {
+		return
+	}
+
+	for row := 0; row < y; row++ {
+		for col := 0; col < x; col++ {
+			if row == 0 || col == y-1 {
+				if col == 0 {
+					fmt.Print("A")
+				} else if col == x-1 {
+					fmt.Print("C")
+				} else {
+					fmt.Print("B")
+				}
+			} else {
+				if col == 0 || col == x-1 {
+					fmt.Print("B")
+				} else {
+					fmt.Print(" ")
+				}
+			}
+		}
+	}
+	fmt.Println()
+}
