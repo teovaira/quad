3518e4b1962ee2bb92c5428effb93fd60f473262
ggkounta <georgiagount@gmail.com>
2025-09-07 14:09:47 +0300

    test

--- QuadE.go
+++ QuadE.go
@@ -11,13 +11,20 @@ func QuadE(x, y int) {
 		for col := 0; col < x; col++ {
 			if row == 0 || row == y-1 {
 				if col == 0 {
+					fmt.Print("A")
+				} else if col == x-1 {
+					fmt.Print("C")
+				} else {
+					fmt.Print("B")
+				}
+			} else if row == 1 || row == y-1 {
+				if col == 0 {
 					fmt.Print("C")
 				} else if col == x-1 {
 					fmt.Print("A")
 				} else {
 					fmt.Print("B")
 				}
-			} else {
 				if col == 0 || col == x-1 {
 					fmt.Print("B")
 				} else {
